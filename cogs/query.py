ac3497ff4e0baa7fba15dbdb006204ef78ca55a4
rudejerk <9592857+rude-jerk@users.noreply.github.com>
2022-07-24 16:14:44 -0500

    Update raid description

--- cogs/query.py
+++ cogs/query.py
@@ -1,19 +1,25 @@
+import logging
+
 from disnake import ApplicationCommandInteraction as Inter, Embed
 from disnake.ext import commands
 
-from config import BOT_MESSAGES
+from config import BOT_MESSAGES, LOG_NAME
 from configs.raids import encounter_detail, wing_detail, wing_encounter_map
 from utils.api import has_required_permissions, get_account_raids
 from utils.users import get_user_by_discord_id
+
+logger = logging.getLogger(LOG_NAME)
 
 
 class Query(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name='raid')
+    @commands.slash_command(name='raid', description='Current raid encounter status. Requires /tregister with an API '
+                                                     'key with account permissions')
     async def raid_encounter_detail(self, inter: Inter):
         await inter.response.defer(with_message=True)
+        logger.info(f"/raid from {inter.user.display_name} [{inter.user.id}]")
         api_perms = ['account']
 
         db_user = get_user_by_discord_id(str(inter.user.id))
@@ -35,10 +41,12 @@ class Query(commands.Cog):
         for wing, encounters in wing_encounter_map.items():
             encounter_values = []
             for encounter in encounters:
-                v = f"{'💀' if encounter in completed_encounters else '❌'}{encounter_detail[encounter]}"
+                v = f"{'💀' if encounter in completed_encounters else '🟢'} {encounter_detail[encounter]}"
                 encounter_values.append(v)
             wing_name = f"{wing_detail[wing]['wing']}: {wing_detail[wing]['name']}"
             raid_embed.add_field(wing_name, value='\n'.join(encounter_values))
+
+        raid_embed.set_footer(text='🟢: Alive  💀: Killed')
 
         await inter.followup.send(embed=raid_embed)
 
