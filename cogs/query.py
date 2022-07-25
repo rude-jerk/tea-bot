53e0c716a09cf4c3be131dd2718203a61a80c7cf
rudejerk <9592857+rude-jerk@users.noreply.github.com>
2022-07-24 21:23:56 -0500

    Add description to private param in raid

--- cogs/query.py
+++ cogs/query.py
@@ -1,5 +1,4 @@
 import logging
-from enum import Enum
 
 from disnake import ApplicationCommandInteraction as Inter, Embed
 from disnake.ext import commands
@@ -18,7 +17,10 @@ class Query(commands.Cog):
 
     @commands.slash_command(name='raid', description='Current raid encounter status. Requires /tregister with an API '
                                                      'key with account permissions')
-    async def raid_encounter_detail(self, inter: Inter, private: bool = True):
+    async def raid_encounter_detail(self, inter: Inter, private: bool = commands.Param(default=True,
+                                                                                       description='Send raid progress '
+                                                                                                   'privately. Default'
+                                                                                                   ': True')):
         await inter.response.defer(with_message=True, ephemeral=True if private else False)
         logger.info(f"/raid from {inter.user.display_name} [{inter.user.id}]")
         api_perms = ['account']
